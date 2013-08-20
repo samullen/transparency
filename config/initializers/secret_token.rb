6af5a8db1cf5ceb6ec9b1390bb0afc17cb82d2d3
Samuel Mullen <samullen@gmail.com>
2013-08-20 15:18:11 -0500

    initial offering

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Transparency::Application.config.secret_key_base = 'dcb69757e25a64054c8a2b9c6ce37401a91021b274839f5bb12611281d06f2d078d54d7780a4fb2443998c3e4746b8e695f1be188f41833ac71f897cf243d0ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Transparency::Application.config.session_store :cookie_store, key: '_transparency_session'
