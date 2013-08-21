d8656cf27a52724e1e3b99a8f19dc9b8d65fe692
Samuel Mullen <samullen@gmail.com>
2013-08-21 15:24:47 -0500

    add basic associations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ActiveRecord::Base
+  belongs_to :user
+
+  has_many :tasks, :dependent => :destroy
+
   validates :user_id, :presence => true
   validates :name, :presence => true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ActiveRecord::Base
+  belongs_to :project
+  belongs_to :category
+
+  validates :project_id, :presence => true
+  validates :name, :presence => true
 end
