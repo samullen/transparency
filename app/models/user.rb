47309cb12a0196d0ea7550ecfc470b43c79c0844
Samuel Mullen <samullen@gmail.com>
2013-08-21 21:48:25 -0500

    add projects association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, 
          :validatable, :token_authenticatable
+
+  has_many :projects
 end
