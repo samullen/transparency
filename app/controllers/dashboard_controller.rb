570b823da100d511d885302b1cff0c7af376a0dd
Samuel Mullen <samullen@gmail.com>
2013-08-22 21:11:34 -0500

    display table of hours for days on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def show
-    @projects = current_user.projects
+    @project_windows = ProjectWindow.for_projects(current_user.projects)
+    @project_analytic = ProjectAnalytic.new(@project_windows)
   end
 end
