477c791d203aeceee9d1d7a2b756f02e0b40a011
Samuel Mullen <samullen@gmail.com>
2013-08-24 23:10:17 -0500

    add remote_id to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130821201838) do
+ActiveRecord::Schema.define(version: 20130825040819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,12 +52,12 @@ ActiveRecord::Schema.define(version: 20130821201838) do
   add_index "tasks", ["started_at"], name: "index_tasks_on_started_at", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                               null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                                           null: false
+    t.string   "encrypted_password",                 default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
+    t.integer  "sign_in_count",                      default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -65,10 +65,12 @@ ActiveRecord::Schema.define(version: 20130821201838) do
     t.string   "authentication_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remote_id",              limit: 128
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remote_id"], name: "index_users_on_remote_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
