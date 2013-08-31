ff386f23e31d0fc8c5a33f03e3b0ee740363cce9
Samuel Mullen <samullen@gmail.com>
2013-08-30 22:09:41 -0500

    add ::accessible_by to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,8 @@ class Project < ActiveRecord::Base
 
   validates :user_id, :presence => true
   validates :name, :presence => true
+
+  def self.accessible_by(user)
+    user.admin? ? self.all : self.where("user_id = ?", user.id)
+  end
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -2,11 +2,26 @@ $LOAD_PATH.unshift(File.expand_path("../..", __FILE__))
 require "test_helper"
 
 describe Project do
-  before do
-    @project = Project.new(:name => "Example Project", :user_id => 1001)
-  end
+  describe "::accessible_by" do
+    before do
+      project_factory = ProjectFactory.new
+      @user = OpenStruct.new(:id => 1001, :admin? => false)
+      @owned_project = project_factory.create(:user_id => 1001)
+      @unowned_project = project_factory.create(:user_id => 2002)
+    end
 
-  it "must be valid" do
-    @project.valid?.must_equal true
+    it "includes projects only belonging to a user" do
+      Project.accessible_by(@user).must_include @owned_project
+    end
+
+    it "excludes projects not owned by a user" do
+      Project.accessible_by(@user).wont_include @unowned_project
+    end
+
+    it "incldues all projects if user is an admin" do
+      admin = OpenStruct.new(:id => 1, :admin? => true)
+      Project.accessible_by(admin).must_include @owned_project
+      Project.accessible_by(admin).must_include @unowned_project
+    end
   end
 end
