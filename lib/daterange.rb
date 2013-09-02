cff07dcfc4cb0cef964a9d127727a2ebff712acc
Samuel Mullen <samullen@gmail.com>
2013-09-01 21:11:03 -0500

    add #[] to daterange calss

--- lib/daterange.rb
+++ lib/daterange.rb
@@ -11,10 +11,18 @@ class Daterange
   end
 
   def each
-    @current_time = @start_time
-    while @current_time <= @end_time
-      yield @current_time
-      @current_time += ONEDAY
+    current_time = self.start_time
+    while current_time <= self.end_time
+      yield current_time
+      current_time += ONEDAY
+    end
+  end
+
+  def [](index)
+    index *= ONEDAY
+
+    if self.start_time + index <= self.end_time
+      self.start_time + index
     end
   end
 end
--- test/lib/daterange_test.rb
+++ test/lib/daterange_test.rb
@@ -14,9 +14,25 @@ describe Daterange do
 
   describe "#each" do
     it "calls the block for each day in the timeframe" do
-      timeframe = Daterange.new(Time.parse("20130801"), Time.parse("20130805"))
-      proc { timeframe.each { puts "x" } }.must_output "x\n" * 5
+      daterange = Daterange.new(Time.parse("20130801"), Time.parse("20130805"))
+      proc { daterange.each { puts "x" } }.must_output "x\n" * 5
     end
   end
 
+  describe "#[]" do
+    it "returns the date at the index provided" do
+      start_time = Time.parse("20130801")
+      end_time = Time.parse("20130805")
+      daterange = Daterange.new(start_time, end_time)
+      daterange[0].must_equal start_time
+      daterange[1].must_equal (start_time + 60 * 60 * 24)
+    end
+
+    it "returns nil if nothing is found" do
+      start_time = Time.parse("20130801")
+      end_time = Time.parse("20130805")
+      daterange = Daterange.new(start_time, end_time)
+      daterange[10].must_be_nil
+    end
+  end
 end
